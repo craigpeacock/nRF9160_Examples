e8a214a43991951573320f6d27bfbd15265d72eb
Craig Peacock <craigp@beyondlogic.org>
2022-04-09 12:32:22 +0930

    Added Basic Networking Example

---
+++ BasicNetworking/src/main.c
@@ -0,0 +1,83 @@
+
+#include <zephyr.h>
+#include <sys/printk.h>
+#include <modem/lte_lc.h>
+#include <modem/modem_info.h>
+
+int print_modem_info(enum modem_info info)
+{
+	int len;
+	char buf[80];
+
+	switch (info) {
+		case MODEM_INFO_RSRP:
+			printk("Signal Strength: ");
+			break;
+		case MODEM_INFO_IP_ADDRESS:
+			printk("IP Addr: ");
+			break;
+		case MODEM_INFO_FW_VERSION:
+			printk("Modem FW Ver: ");
+			break;
+		case MODEM_INFO_ICCID:
+			printk("SIM ICCID: ");
+			break;
+		case MODEM_INFO_IMSI:
+			printk("IMSI: ");
+			break;
+		case MODEM_INFO_IMEI:
+			printk("IMEI: ");
+			break;
+		case MODEM_INFO_DATE_TIME:
+			printk("Network Date/Time: ");
+			break;
+		case MODEM_INFO_APN:
+			printk("APN: ");
+			break;
+	}
+
+	len = modem_info_string_get(info, buf, 80);
+	if (len > 0) {
+		printk("%s\n",buf);
+    	//printk("%s (%d bytes)\n",buf,len);
+	} else {
+		printk("Error\n");
+	}
+}
+
+void main(void)
+{
+	int err;
+
+	printk("\nnRF9160 Basic Networking Example (%s)\n", CONFIG_BOARD);
+
+	err = lte_lc_init();
+	if (err) printk("MODEM: Failed initializing LTE Link controller, error: %d\n", err);
+
+	err = lte_lc_func_mode_set(LTE_LC_FUNC_MODE_ACTIVATE_UICC);
+	if (err) printk("MODEM: Failed enabling UICC power, error: %d\n", err);
+	k_msleep(100);
+	
+	err = modem_info_init();
+	if (err) printk("MODEM: Failed initializing modem info module, error: %d\n", err);
+
+	print_modem_info(MODEM_INFO_FW_VERSION);
+	print_modem_info(MODEM_INFO_IMEI);
+	//print_modem_info(MODEM_INFO_ICCID);
+	printk("\n");
+
+	printk("Waiting for network... ");
+	err = lte_lc_init_and_connect();
+	if (err) {
+		printk("Failed to connect to the LTE network, err %d\n", err);
+		return;
+	}
+	printk("OK\n");
+	print_modem_info(MODEM_INFO_APN);
+	print_modem_info(MODEM_INFO_IP_ADDRESS);
+	//print_modem_info(MODEM_INFO_DATE_TIME);
+	//print_modem_info(MODEM_INFO_RSRP);
+	
+}
+
+
